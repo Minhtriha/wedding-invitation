4aaafdf42a249b1bc69b19228d6de1092a6c0b21
Minh Tri <minhtriha@gmail.com>
2026-08-28 20:27:40 +0700

    Final mau1: bo thoi gian, chinh text can giua thoang, giu bo goc 30px

--- make_og.ps1
+++ make_og.ps1
@@ -8,26 +8,18 @@ $W = 1200; $H = 630
 
 $cream    = [System.Drawing.Color]::FromArgb(253,250,243)
 $brown    = [System.Drawing.Color]::FromArgb(84,64,52)
-$brownSoft= [System.Drawing.Color]::FromArgb(150,120,100)
-$gold     = [System.Drawing.Color]::FromArgb(179,138,82)
-$white    = [System.Drawing.Color]::White
 $goldLine = [System.Drawing.Color]::FromArgb(200,176,128)
 
 $fScript = New-Object System.Drawing.Font('Georgia',34,[System.Drawing.FontStyle]::Bold)
 $fSerifD = New-Object System.Drawing.Font('Georgia',50,[System.Drawing.FontStyle]::Bold)
-$fSmall  = New-Object System.Drawing.Font('Segoe UI',20,[System.Drawing.FontStyle]::Regular)
-$fDate   = New-Object System.Drawing.Font('Segoe UI',24,[System.Drawing.FontStyle]::Bold)
 
-$sfL = New-Object System.Drawing.StringFormat
-$sfL.Alignment = 'center'; $sfL.LineAlignment = 'center'
 $sfR = New-Object System.Drawing.StringFormat
 $sfR.Alignment = 'center'; $sfR.LineAlignment = 'center'
 
 # Tiếng Việt qua unicode escape
-$tT1 = [char]0x54+[char]0x68+[char]0x69+[char]0x1EC7+[char]0x70+' '+[char]0x4D+[char]0x1EDD+[char]0x69+' '+[char]0x43+[char]0x1B0+[char]0x1EDB+[char]0x69   # Thiệp Mời Cưới
-$tBT = [char]0x42+[char]0x1EA3+[char]0x6F+' '+[char]0x54+[char]0x72+[char]0xE2+[char]0x6E   # Bảo Trân
-$tMT = [char]0x4D+[char]0x69+[char]0x6E+[char]0x68+' '+[char]0x54+[char]0x72+[char]0xED   # Minh Trí
-$tDate = '19.09.2026 ' + [char]0xB7 + ' 09:00'
+$tT1 = [char]0x54+[char]0x68+[char]0x69+[char]0x1EC7+[char]0x70+' '+[char]0x4D+[char]0x1EDD+[char]0x69+' '+[char]0x43+[char]0x1B0+[char]0x1EDB+[char]0x69
+$tBT = [char]0x42+[char]0x1EA3+[char]0x6F+' '+[char]0x54+[char]0x72+[char]0xE2+[char]0x6E
+$tMT = [char]0x4D+[char]0x69+[char]0x6E+[char]0x68+' '+[char]0x54+[char]0x72+[char]0xED
 
 function DrawText($g,$t,$f,$c,$x,$y,$px,$py){
   $br = New-Object System.Drawing.SolidBrush($c)
@@ -49,45 +41,31 @@ function SaveJ($bmp,$name,$q){
   Write-Output ('OK '+$name)
 }
 
-# ---- MẪU 1 TINH CHỈNH ----
+# ---- MẪU 1 FINAL: bỏ thời gian, bo góc ảnh 30px, viền vàng mảnh, text căn giữa thoáng ----
 $b = New-Object System.Drawing.Bitmap($W,$H); $g = [System.Drawing.Graphics]::FromImage($b)
 $g.Clear($cream)
 $g.SmoothingMode = 'AntiAlias'; $g.TextRenderingHint = [System.Drawing.Text.TextRenderingHint]::AntiAliasGridFit
 
-# Khung viền vàng mảnh
 $pen = New-Object System.Drawing.Pen($goldLine); $pen.Width = 2
 $g.DrawRectangle($pen,22,22,$W-44,$H-44)
 
-# Ảnh hero - khối trái, bo phần top, giữ đầu
 $imgX=70; $imgY=55; $imgW=430; $imgH=518
-# clip theo hình tròn nhẹ ở cạnh (bo góc)
 $path = New-Object System.Drawing.Drawing2D.GraphicsPath
-$path.AddArc($imgX,$imgY,30,30,180,90); $path.AddArc($imgX+$imgW-30,$imgY,30,30,270,90)
-$path.AddArc($imgX+$imgW-30,$imgY+$imgH-30,30,30,0,90); $path.AddArc($imgX,$imgY+$imgH-30,30,30,90,90); $path.CloseFigure()
+[void]$path.AddArc($imgX,$imgY,30,30,180,90)
+[void]$path.AddArc($imgX+$imgW-30,$imgY,30,30,270,90)
+[void]$path.AddArc($imgX+$imgW-30,$imgY+$imgH-30,30,30,0,90)
+[void]$path.AddArc($imgX,$imgY+$imgH-30,30,30,90,90); [void]$path.CloseFigure()
 $g.SetClip($path)
 DrawCover $g $hero $imgX $imgY $imgW $imgH
 $g.ResetClip()
 
-# Cột text phải
 $textX = 570; $textW = 560
-
-# Dòng "Thiệp Mời Cưới" - script nhỏ, có gạch nhỏ mờ hai bên
-$g.DrawString($tT1, $fScript, (New-Object System.Drawing.SolidBrush($gold)), (New-Object System.Drawing.RectangleF($textX,90,$textW,55)), $sfR)
-
-# Gạch trang trí ngắn giữa
+$g.DrawString($tT1, $fScript, (New-Object System.Drawing.SolidBrush($goldLine)), (New-Object System.Drawing.RectangleF($textX,90,$textW,55)), $sfR)
 $linePen = New-Object System.Drawing.Pen($goldLine); $linePen.Width = 1
 $g.DrawLine($linePen, 620, 175, 620+560-100, 175)
 
-# "Bảo Trân" lớn
-DrawText $g $tBT $fSerifD $brown $textX 195 $textW 85
-# "Minh Trí" lớn
-DrawText $g $tMT $fSerifD $brown $textX 295 $textW 85
-
-# Gạch thứ 2
-$g.DrawLine($linePen, 620, 405, 620+560-4-100, 405)
-
-# Ngày giờ
-DrawText $g $tDate $fDate $brownSoft $textX 430 $textW 60
+DrawText $g $tBT $fSerifD $brown $textX 230 $textW 90
+DrawText $g $tMT $fSerifD $brown $textX 345 $textW 90
 
 SaveJ $b 'mau1-final.jpg' 95
 $hero.Dispose()
